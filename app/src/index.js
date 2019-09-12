9b6443c662b60a36205088f7daf594ee97ff331d
Tiago Cardoso <tcardoso@gmail.com>
2019-09-12 23:22:16 +0800

    Working on stamina gauge element conversion (WIP)

--- app/src/index.js
+++ app/src/index.js
@@ -71,11 +71,69 @@ class StartGameButton extends React.Component {
   }
 }
 
+class StaminaBar extends React.Component {
+  /*constructor(props) {
+    super(props);
+    this.state = props.value
+    /*let stamina = ctx.match.playerStats[player].stamina
+    $(`#fieldPlayer_${player} > div.value`).css({
+        width: stamina/2,
+        backgroundColor: stamina < 55 ? (stamina < 20 ? 
+            settings().game.gauges.veryLowStaminaColor : 
+            settings().game.gauges.lowStaminaColor) : 
+            settings().game.gauges.highStaminaColor
+    })
+}*/
+
+  render() {
+    //alert(JSON.stringify(this.props.player))
+    return (
+      <li id={ this.props.player.obj } style={{left: this.props.player.x-30, top: this.props.player.y+30}}>
+        <div className="value">{ this.props.player.stamina }</div>
+      </li>
+    );
+  }
+}
+
+class StaminaBars extends React.Component {
+  /*constructor(props) {
+    super(props);
+    this.state = props.players
+    console.log(props)
+    if (props && Array.isArray(props)) {
+      alert(JSON.stringify(props))
+      this.players = props.players;
+      //Object.keys(props.players).forEach(name => {
+        //alert(name)
+      //})  
+    }
+
+    this.state = props.players //Should the state be this?
+  }*/
+
+  renderStaminaBar(obj) {
+    //alert(JSON.stringify(obj))
+    return <StaminaBar player={obj} />;
+  }
+
+  render() {
+    let items = null
+    if(this.props.players) {
+      items = this.props.players.map((p) => this.renderStaminaBar(p))
+    }
+    return (
+      <ul className="stamina-bars">
+        {items}
+      </ul>
+    )
+  }
+}
+
 class MatchClock extends React.Component {
   render() {
     return (
       <div className="clock-container"><div className="clock">{this.props.time}</div><div className="half">1st half</div></div>
-    );
+    )
   }
 }
 
@@ -86,19 +144,27 @@ class Scene extends React.Component {
       displayMainScene: false,
       time: ""
     };
-    this.setTime = (ctx) => {
+    this.tickHandler = (ctx) => {
       if(!ctx.matchIsPaused()) {
-        let display = this.state.displayMainScene
+        //alert(JSON.stringify(ctx.getScene().getActors()[1].attr.x()))
         this.setState({
-          displayMainScene: display,
-          time: ctx.updateClock()
+          displayMainScene: this.state.displayMainScene,
+          time: ctx.updateClock(),
+          players: ctx.getScene().getActors().map((p) => {
+            return {
+              obj: p.obj,
+              stamina: ctx.match.playerStats["Hyuga"].stamina, //Replace by proper
+              x: p.pos.x(),
+              y: p.pos.y()
+            }
+          })
         })
       }
     }
-    this.handler = this.handler.bind(this)
+    this.displayHandler = this.displayHandler.bind(this)
   }
 
-  handler() {
+  displayHandler() {
     this.setState({
       displayMainScene: !this.state.displayMainScene
     })
@@ -112,7 +178,7 @@ class Scene extends React.Component {
   initializeCanvas() {
     let that = this
     setTimeout(() => {
-      drawMainCanvas(that.setTime)
+      drawMainCanvas(that.tickHandler)
     }, 10)
   }
 
@@ -129,14 +195,15 @@ class Scene extends React.Component {
         <div className="rightbox"></div>
         <div className="leftbox"></div>
         <div className="footer-comments"></div>
-        <MatchClock time={this.state.time} />
+        <MatchClock time = {this.state.time} />
+        <StaminaBars players = {this.state.players} />
         <ul className="stamina-bars">
         </ul>
       </div>
       )
     } else {
       return (
-        <StartGameButton handler = {this.handler} />
+        <StartGameButton handler = {this.displayHandler} />
       )
     }
   }
